1710c78317fa49ef8da350f72f1a33fb5f9502aa
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2026-07-05 00:07:28 +0530

    Spring Boot Project Scaffold

---
+++ astro-sentinel/backend/astrosentinel/src/main/java/dev/vvr/astrosentinel/AstrosentinelApplication.java
@@ -0,0 +1,13 @@
+package dev.vvr.astrosentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrosentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrosentinelApplication.class, args);
+	}
+
+}
---
+++ astro-sentinel/backend/astrosentinel/src/test/java/dev/vvr/astrosentinel/AstrosentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vvr.astrosentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrosentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
